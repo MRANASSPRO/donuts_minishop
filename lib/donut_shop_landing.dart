22271df8afdc6475f7829dcbd7a94049be1424e4
Anass Radi <mranassradi@gmail.com>
2021-11-27 01:28:11 +0100

    separate ui widgets into separate classes and add app bottom bar

--- lib/donut_shop_landing.dart
+++ lib/donut_shop_landing.dart
@@ -1,13 +1,31 @@
+import 'package:donuts_minishop/ui/donut_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'utils.dart';
+import 'package:donuts_minishop/ui/donut_side_menu.dart';
 
 class DonutShopLanding extends StatelessWidget {
   const DonutShopLanding({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, Donut Shop!'),
+    return Scaffold(
+      drawer: const Drawer(
+        child: DonutSideMenu(),
+      ),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Utils.mainDark),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Image.network(Utils.donutLogoRedText, width: 120),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Container() //placeholder for Navigator
+              ),
+          const DonutBottomBar()
+          //placeholder for BottomBar
+        ],
       ),
     );
   }
